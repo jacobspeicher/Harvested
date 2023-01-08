3f84cf2018e07513a5567d3fef6146f59266ad2b
SHestres <sidneyhestres@gmail.com>
2023-01-08 16:41:08 -0600

    Grassy Fields

---
+++ Assets/UI/Menu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject MainMenu;
+    public GameObject CreditMenu;
+    private void Start()
+    {
+        /*
+        AudioManager.Instance.Play("MenuMusic");
+        try
+        {
+            AudioManager.Instance.gameObject.GetComponentInChildren<MusicHandler>().stopMusic();
+        }
+        catch { }
+        */
+    }
+
+    public void PlayButton()
+    {
+        Play();
+    }
+
+    public void Play()
+    {
+        StartCoroutine(startGame());
+    }
+
+    IEnumerator startGame()
+    {
+        yield return new WaitForEndOfFrame();
+        //AudioManager.Instance.Stop("MenuMusic");
+        //AudioManager.Instance.gameObject.GetComponentInChildren<MusicHandler>().playMusic();
+        MainMenu.SetActive(false);
+
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    public void Credits()
+    {
+        MainMenu.SetActive(false);
+        CreditMenu.SetActive(true);
+    }
+
+    public void ReturnToMain()
+    {
+        CreditMenu.SetActive(false);
+        MainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
